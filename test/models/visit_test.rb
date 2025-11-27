22136a6d039c4a7ee6f8db65f21d917e7f15e1dd
Sofia14Belle <sofiabeleza928@gmail.com>
2025-11-27 03:25:39 -0300

    Update visit_test.rb

--- test/models/visit_test.rb
+++ test/models/visit_test.rb
@@ -1,7 +1,24 @@
 require "test_helper"
 
 class VisitTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @visitor = Visitor.create!(name: "Pedro", email: "pedro@mail.com")
+  end
+
+  test "registro de visita é válido com visitante" do
+    visit = Visit.new(visitor: @visitor, check_in: Time.now)
+    assert visit.valid?
+  end
+
+  test "visita aberta sem check_out é válida" do
+    visit = Visit.create!(visitor: @visitor, check_in: Time.now)
+    assert_nil visit.check_out
+    assert visit.valid?
+  end
+
+  test "finalização de visita define check_out" do
+    visit = Visit.create!(visitor: @visitor, check_in: Time.now)
+    visit.update(check_out: Time.now + 1.hour)
+    assert_not_nil visit.check_out
+  end
 end
