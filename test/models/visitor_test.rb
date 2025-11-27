453ffc437aa079341418bdc52d932b599e5fa3eb
Sofia14Belle <sofiabeleza928@gmail.com>
2025-11-27 03:23:58 -0300

    Update visitor_test.rb

--- test/models/visitor_test.rb
+++ test/models/visitor_test.rb
@@ -1,7 +1,25 @@
 require "test_helper"
 
 class VisitorTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "válido com campos obrigatórios" do
+    visitor = Visitor.new(
+      name: "Maria Silva",
+      email: "maria@mail.com"
+    )
+    assert visitor.valid?
+  end
+
+  test "inválido sem nome" do
+    visitor = Visitor.new(email: "test@mail.com")
+    assert_not visitor.valid?
+    assert_includes visitor.errors[:name], "can't be blank"
+  end
+
+  test "email inválido deve falhar" do
+    visitor = Visitor.new(
+      name: "João",
+      email: "email_invalido"
+    )
+    assert_not visitor.valid?
+  end
 end
